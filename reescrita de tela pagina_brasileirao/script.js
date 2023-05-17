8e1ce7c096c03dadcb65f6e41e5e7312750b6752
Daniel Kloh <103609213+DanielKloh@users.noreply.github.com>
2023-05-16 21:51:26 -0300

    Add files via upload

---
+++ reescrita de tela pagina_brasileirao/script.js
@@ -0,0 +1,23 @@
+let ativado = false;
+
+function menu() {
+
+    if (ativado === false) {
+        ativado = true;
+        abrir_menu();
+    }
+    else {
+        ativado = false;
+        fehecar_menu();
+    }
+}
+
+function abrir_menu() {
+    document.getElementById("show_menu").style.width = "250px";
+    document.getElementById("fundo").classList.add("show_fundo");
+}
+
+function fehecar_menu() {
+    document.getElementById("show_menu").style.width = "0px";
+    document.getElementById("fundo").classList.remove("show_fundo");
+}
